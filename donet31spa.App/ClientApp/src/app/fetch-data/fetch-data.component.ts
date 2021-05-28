813019d28faefca44b2129851e592bfb0354214b
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2021-05-28 13:23:48 -0400

    Add comments fo fetch-data component

--- donet31spa.App/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ donet31spa.App/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -4,19 +4,35 @@ import { WeatherForecast } from './fetch-data.models';
 import { FetchDataService } from './fetch-data.service';
 import { Router } from '@angular/router';
 
+
 @Component({
     selector: 'app-fetch-data',
     templateUrl: './fetch-data.component.html',
     styleUrls: ['./fetch-data.component.scss']
 })
+/**
+ * Responsible to show weather reports.
+ * Provides a way to access edit and delete weather report features.
+ */
 export class FetchDataComponent implements OnInit {
+    /** Array of weather forecasts. */
     forecasts: WeatherForecast[];
 
+    /** Angular materials columns configuration. */
     displayedColumns: string[] = ['id', 'date', 'temperatureC', 'temperatureF', 'summary', 'actions'];
 
+    /**
+     * FetchDataComponent Constructor
+     * @param fetchDataService Responsible for the rest api communication.
+     * @param router Provides navigation among views and URL manipulation capabilities
+     */
     constructor(private fetchDataService: FetchDataService, private router: Router) { }
 
+    /**
+     * OnInit Angular Hook
+     */
     ngOnInit(): void {
+        // Gets the report
         this.fetchDataService.getWeatherReport().subscribe((result) => {
             this.forecasts = result;
         }, (error) => {
@@ -25,10 +41,19 @@ export class FetchDataComponent implements OnInit {
         });
     }
 
+    /**
+     * Provides access to the edit feature
+     * @param weather Weather forecast
+     */
     edit(weather: WeatherForecast) {
+        // Redirects user to the weather form
         this.router.navigate(['weather-form', weather.id]);
     }
 
+    /**
+    * Deletes a give report.
+    * @param weather Weather forecast
+    */
     async delete(weather: WeatherForecast) {
         this.fetchDataService.deleteWeatherReport(weather).subscribe(() => {
             this.forecasts = this.forecasts.filter((w) => w.id !== weather.id)
